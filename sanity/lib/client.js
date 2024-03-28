e0a3e817c489f654bfb89c2ad7e724bd5488c040
tim <timothyshih12032007@gmail.com>
2024-03-28 11:34:09 +0800

    env setting for deploy

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,14 +1,14 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-import { apiVersion, dataset, projectId, useCdn, token } from '../env'
+//import { apiVersion, dataset, projectId, useCdn, token } from '../env'
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-  token,
+  apiVersion: '2024-03-02',
+  dataset: 'production',
+  projectId: '492tg9zk',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
 
 const builder = imageUrlBuilder(client);
